f701cd62016536c955235979ac70986f9e6d461e
Jeel Dobariya <jeeldobariya38@gmail.com>
2025-08-16 15:04:18 +0530

    feat: improve announcement page by adding banner

--- src/app/page.js
+++ src/app/page.js
@@ -2,24 +2,32 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 justify-center items-center">
+    <div className="font-sans flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] justify-center items-center">
+        <Image
+          className="dark:invert"
+          src="/passcodes-banner.png"
+          alt="Passcodes logo"
+          width={1024 / 2}
+          height={500 / 2}
+          priority
+        />
         <div className="flex flex-col sm:flex-row justify-evenly items-center my-4 bg-slate-900 text-pink-500 rounded p-7">
           <div className="px-4">
             <Image
               className="dark:invert"
               src="/passcodes.png"
               alt="Passcodes logo"
-              width={200}
-              height={100}
+              width={512 / 4}
+              height={512 / 4}
               priority
             />
           </div>
-          <div>
-            <div className="pb-6 text-4xl font-black">
+          <div className="p-4">
+            <div className="pb-6 text-lg font-black sm:text-4xl">
               <h1>🥳🥳 Coming Soon!! 🎉🎉</h1>
             </div>
-            <div className="text-lg text-cyan-900">
+            <div className="text-xs text-cyan-900 sm:text-lg">
               <p>
                 Passcodes Website & App will be offically release on <b>17/8</b>
                 .
